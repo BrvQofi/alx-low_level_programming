4d4a014d6c3caca936ef6f66ffbfd61d787d7d5e
BrvQofi <ericowusu196@gmail.com>
2023-08-16 04:27:51 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,7 @@
 
 
 	/**
-	 * _isalpha - checks for alphabetic character
+	 * _isalpha - prints alphabets only
 	 * @c: the character to be checked
 	 * Return: 1 if c is a letter, 0 otherwise
 	 */
